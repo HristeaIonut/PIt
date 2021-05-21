1820f95d31d8862a3bbaefd359e0abacd50714f7
Hristea Ionut <hristeaionut72@gmail.com>
2021-05-21 15:58:43 +0300

    smol fix

--- controller/getUsername.php
+++ controller/getUsername.php
@@ -1,12 +1,18 @@
 <?php
+session_start();
 require ("generatekeys.php");
 require ("connection/connection.php");
     global $cipher, $key, $iv, $tag, $conn;
-    $decryptedId = openssl_decrypt($_COOKIE["user_login"], $cipher, $key, $options = 0, $iv);
-    $sql = "SELECT * FROM connectiontable where user_id=? ";
-    $stmt = $conn->prepare($sql);
-    $stmt -> bind_param("s", $decryptedId);
-    $stmt -> execute();
-    $result = $stmt->get_result();
-    $row = $result->fetch_assoc();
-    echo $row["user_name"];
+    if(isset($_COOKIE["user_loing"])){
+        $decryptedId = openssl_decrypt($_COOKIE["user_login"], $cipher, $key, $options = 0, $iv);
+        $sql = "SELECT * FROM connectiontable where user_id=? ";
+        $stmt = $conn->prepare($sql);
+        $stmt -> bind_param("s", $decryptedId);
+        $stmt -> execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+        echo $row["user_name"];
+    }
+    else{
+        echo $_SESSION["username"];
+    }
